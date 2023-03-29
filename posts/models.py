e7473d2e8144a28cd1885204efb5380e9512542f
Marcelo Marques <marcelo-mls@hotmail.com>
2023-03-29 01:53:48 -0300

    feat: backend with DRF

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from users.models import User
+
+
+class Post(models.Model):
+    post = models.CharField(max_length=512)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.post
---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=45)
+    initials = models.CharField(max_length=2)
+    email = models.EmailField()
+    encrypted_password = models.CharField(max_length=45)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
